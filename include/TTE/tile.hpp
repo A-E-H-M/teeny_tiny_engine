7a73d3ab3a37a892ea10e93a8f379b3096ebeb76
alexandria <alexandria.hmann@gmail.com>
2026-01-10 14:21:05 -0600

    Updated CMakeLists

--- include/TTE/tile.hpp
+++ include/TTE/tile.hpp
@@ -4,41 +4,62 @@
 #include <vector>
 #include <memory>
 
-enum class Colors
+#include <SFML/Graphics/RectangleShape.hpp>
+
+#include <TTE/vec2.hpp>
+
+namespace TTE 
 {
-    WHITE,
-    LIGHT_GREY,
-    DARK_GREY,
-    BLACK
-};
+    enum class Colors
+    {
+        WHITE,
+        LIGHT_GREY,
+        DARK_GREY,
+        BLACK
+    };
 
-struct Tiny_box
-{
-    // set default value for color
-    long long hex_color;
-    int ID;
-};
+    // Represents a pixel, color as RGB
+    struct Tiny_box
+    {
+        sf::RectangleShape tiny_box;
 
-class Tile
-{
-    std::vector<std::unique_ptr<Tiny_box>> tile;
+        Tiny_box(std::uint8_t red = 100, std::uint8_t green = 90, std::uint8_t blue = 280, std::uint8_t alpha = 255) :
+        {
+            tiny_box.setSize(Vec2(100, 50));
+            tiny_box.setFillColor(sf::Color(red, blue, green, alpha));
+        };
+    };
 
-    int Tile_ID;
+    // Represents an 8 x 8 block of pixels
+    class Tile
+    {
+        std::vector<Tiny_box> tile_row;
 
-};
+        int Tile_ID;
+        Tile() :
+        {
+            for (int 0 = i; i < 8; i++)
+            {
+                tile_row.push(new Tiny_box);
+            }
+        }
 
-class Tile_map
-{
-    std::vector<Tile> tile_map;
-};
+    };
 
-class Tile_manager
-{
-    std::vector<std::unique_ptr<Tile>> Tile_map_grid;
-    
-    std::unique_ptr<Tile_map> create_tile_map();
-    std::unique_ptr<Tile> create_tile();
+    // Represents a map of all tiles
+    class Tile_map
+    {
+        std::vector<Tile> tile_map;
+    };
 
-    void update_tile_map(const Tile_map* title_map_ptr);
-    void update_tile(const Tile* tile_ptr);
-};
+    class Tile_manager
+    {
+        std::vector<std::unique_ptr<Tile>> Tile_map_grid;
+        
+        std::unique_ptr<Tile_map> create_tile_map();
+        std::unique_ptr<Tile> create_tile();
+
+        void update_tile_map(const Tile_map* title_map_ptr);
+        void update_tile(const Tile* tile_ptr);
+    };
+} // End namespace
