c663813a164ab55baccc800db1e07b14581bed09
alexandria <alexandria.hmann@gmail.com>
2026-04-20 20:23:30 -0500

    Added ring buffer unit testing

---
+++ include/TTE/r_buffer.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <cassert>
+#include <array>
+
+/*
+* It is a buffer that automatically wraps to the beginning of the buffer once the buffer has reached its end.
+*/
+
+namespace R_BUFFER
+{
+    template <typename T, std::size_t size>
+    class Ring_buffer
+    {
+        private:
+            //const std::size_t size;
+            std::array<T, size> Darray;
+
+            //std::size_t counter{0};
+            std::size_t last_pos{0};
+        
+        public:
+            // element access function, returns the oldest element appended to the buffer, read only
+            const T end()
+            {
+                auto temp_end = (last_pos + Darray.size()) % Darray.size();
+                return Darray[temp_end];
+            }
+
+            // element access function, returns the latest element appended to the buffer, read only
+            const T last()
+            {
+                auto temp_last = (last_pos + Darray.size() - 1) % Darray.size();
+                return Darray[temp_last];
+            }
+
+            // element access function, read only
+            const T at(std::size_t index)
+            {
+                assert (index <= Darray.size());
+                assert (index >= 0);
+
+                return Darray[index];
+            }
+
+            // capacity assessment
+            const std::size_t size_of() 
+            {   
+                return Darray.size();
+            };
+
+            // modifier function, clears the contents of the buffer
+            void clear()
+            {
+                Darray.clear();
+            }
+
+            // modifier function, adds element to the next index space
+            void append(const T& element)
+            {
+                Darray[last_pos % size] = element;
+                ++last_pos;
+            }
+
+            // modifier function
+            void assign(std::size_t index, const T& element)
+            {
+                assert (index <= Darray.size());
+                assert (index >= 0);
+                Darray[index] = element;
+            }
+
+    };
+
+} // end of namespace
---
+++ tests/r_buffer_catch_test.cpp
@@ -0,0 +1,107 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <stdexcept>
+#include <iostream>
+
+#include "TTE/r_buffer.hpp"
+
+using namespace R_BUFFER;
+
+TEST_CASE( "Test template for ring buffer", "[ring_buffer template]" )
+{
+    Ring_buffer<int, 3> int_buffer;
+
+    REQUIRE (int_buffer.size_of() == 3);
+
+    SECTION ("Modification using appending")
+    {
+        // Appending to the limit of the buffer without wrapping
+        int_buffer.append(5);
+        int_buffer.append(23);
+        int_buffer.append(17);
+
+        REQUIRE( int_buffer.at(0) == 5 );
+        REQUIRE( int_buffer.at(1) == 23 );
+        REQUIRE( int_buffer.at(2) == 17 );
+
+        // Appending with wrapping
+        int_buffer.append(10);
+        int_buffer.append(63);
+        int_buffer.append(5623);
+
+        REQUIRE ( int_buffer.at(0) == 10 );
+        REQUIRE ( int_buffer.at(1) == 63 );
+        REQUIRE ( int_buffer.at(2) == 5623 );
+
+    }
+
+    SECTION ("Modification at specific index")
+    {
+
+        int_buffer.assign(1, 8);
+        int_buffer.assign(0, 489);
+
+        REQUIRE ( int_buffer.at(1) == 8 );
+        REQUIRE ( int_buffer.at(0) == 489 );
+        
+    }
+    
+	/*
+    SECTION ("Modification by clearing buffer")
+    {
+        int_buffer.clear();
+
+        REQUIRE( int_buffer.at(0) == 5 );
+        REQUIRE( int_buffer.at(1) == 23 );
+        REQUIRE( int_buffer.at(2) == 17 );
+    }
+    */
+
+    SECTION ("Accessing last-in element added to buffer")
+    {
+        // Using append to modify buffer before accessing last-in element
+        int_buffer.append(70);
+        REQUIRE ( int_buffer.last() == 70 );
+
+        int_buffer.append(4953);
+        REQUIRE ( int_buffer.last() == 4953 );
+
+        int_buffer.append(456);
+        REQUIRE ( int_buffer.last() == 456 );
+        
+        // Appending all buffer elements with value 0
+        int_buffer.append(0);
+        REQUIRE ( int_buffer.last() == 0 );
+
+        int_buffer.append(0);
+        REQUIRE ( int_buffer.last() == 0 );
+
+        int_buffer.append(0);
+        REQUIRE ( int_buffer.last() == 0 );
+
+    }
+
+    SECTION ("Accessing first-in element added to buffer")
+    {
+        // Using append to modify buffer before accessing first-in element
+        REQUIRE ( int_buffer.end() == 0 );
+        int_buffer.append(70);
+        
+        REQUIRE ( int_buffer.end() == 0 );
+        int_buffer.append(4953);
+
+        REQUIRE ( int_buffer.end() == 0 );
+        int_buffer.append(37);
+
+        REQUIRE ( int_buffer.end() == 70 );
+        int_buffer.append(0);
+
+        REQUIRE ( int_buffer.end() == 4953 );
+        int_buffer.append(0);
+
+        REQUIRE ( int_buffer.end() == 37 );
+        int_buffer.append(0);
+    }
+
+}
